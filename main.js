c575f5a483fe1fd744780931df3172233f2ad695
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2024-04-30 01:37:50 +0500

    project completion

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+// this shebang command is used to tell the system that this file is a Node.JS script
+// import the "inquirer" module, which is a CLI for Node.JS
+import inquirer from "inquirer";
+// Declare a constant "answers" and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Sentence to Count the words"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// Print the Array of words to the console
+console.log(words);
+// print the length of words of sentence
+console.log(`The number of words in your sentence is ${words.length}`);
